6ef0b48ef8c659f10d2f74e1354a6cb172e909b7
Zihad870 <zihadhossen870@gmail.com>
2025-12-07 00:47:49 +0600

    Array

---
+++ DSA/C++_practice_sauda_apu/array/majorElement.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int majorElement(vector<int>&nums){
+    int freq=0,ans=0;
+    int n=nums.size();
+    for(int i=0;i<n;i++){
+        if(freq==0){
+            ans=nums[i];
+        }
+        if(ans==nums[i]){
+            freq++;
+        }
+        else {
+
+        freq--;
+    }
+    }
+    return ans;
+}
+
+ int main() {
+    vector<int> nums = {2, 2, 1, 1, 1, 2, 2};
+
+    int ans = majorElement(nums);
+
+    cout << "Majority Element = " << ans << endl;
+
+    return 0;
+}
