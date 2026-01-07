72df17138d77f6267d99d03b5ee7d57332c609e4
Zihad870 <zihadhossen870@gmail.com>
2026-01-07 19:13:18 +0600

    Remove All Occurence

---
+++ DSA/C++_practice_sauda_apu/array/String/removeAllOcccurance.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+string removeOccurence(string s,string part){
+    while(s.length()>0&& s.find(part)<s.length()){
+        s.erase(s.find(part),part.length());
+    }
+    return s;
+}
+int main(){
+    string s="daabcbaabcbc",part="abc";
+    cout<<removeOccurence(s,part);
+}
