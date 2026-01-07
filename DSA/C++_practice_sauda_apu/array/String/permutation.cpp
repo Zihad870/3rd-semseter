cfa0f802f75c40f3b60c947c2823c0c9ff9f0da9
Zihad870 <zihadhossen870@gmail.com>
2026-01-07 22:17:49 +0600

    Permutation

---
+++ DSA/C++_practice_sauda_apu/array/String/permutation.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+bool isFreqsame(int freq1[],int freq2[]){
+    for(int i=0;i<26;i++){
+        if(freq1[i]!=freq2[i]){
+            return false;
+        }
+        
+    }
+    return true;
+}
+bool chekInclusion(string s1,string s2){
+    int freq[26]={0};
+    for(int i=0;i<s1.length();i++){
+        freq[s1[i]-'a']++;
+    }
+    int windSize=s1.length();
+    for(int i=0;i<s2.length();i++){
+
+        int windIndex=0,idx=i;
+        int windfreq[26]={0};
+        while(windIndex<windSize&& idx<s2.length()){
+      windfreq[s2[idx-'a']]++;
+      windIndex++;
+      idx++;
+        }
+        if(isFreqsame(freq,windfreq)){
+            return true;
+        }
+    }
+    return false;
+    
+}
+int main (){
+    string s1="ab";
+    string s2="eidbaooo";
+    cout<<chekInclusion(s1,s2)<<endl;
+}
