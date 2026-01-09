e91bd647ad479c9417c9b25f5a7e7e86a700e185
Zihad870 <zihadhossen870@gmail.com>
2026-01-09 16:58:20 +0600

    gcd Euclids algoritm

---
+++ DSA/C++_practice_sauda_apu/array/String/math/EuclidsAlorithmGCD.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int gcd(int a,int b){
+    while(a> 0 && b>0){
+    if(a>b){
+        a=a%b;
+    }
+    else{
+        b=b%a;
+    }
+}
+    if(a==0){
+        return b;
+    }
+    else {
+        return a;
+    }
+    
+}
+int main(){
+    cout<<gcd(20,28)<<endl;
+}
