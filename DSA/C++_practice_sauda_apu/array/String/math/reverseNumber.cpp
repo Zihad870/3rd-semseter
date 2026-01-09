0119c9fa66eea58a5f1be11638243239dfa0368d
Zihad870 <zihadhossen870@gmail.com>
2026-01-09 17:15:27 +0600

    Reverse number

---
+++ DSA/C++_practice_sauda_apu/array/String/math/reverseNumber.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int reverse(int n){
+    int revNum=0;
+    while(n!=0){
+        int dig=n%10;
+        if(revNum>INT32_MAX/10||revNum<INT32_MIN/10){
+            return 0;
+        }
+        revNum=revNum*10+dig;
+        n=n/10;
+        
+    }
+    return revNum;
+}
+int main(){
+    int n=243;
+    cout<<reverse(n)<<endl;
+}
