6f60a0640af1478a6b4a724c83fc6bdcb11dfa17
Zihad870 <zihadhossen870@gmail.com>
2026-01-24 14:28:30 +0600

    Link List Push_front

--- DSA/C++_practice_sauda_apu/LinkList/LinkListPush_back.cpp
+++ DSA/C++_practice_sauda_apu/LinkList/LinkListPush_back.cpp
@@ -20,7 +20,10 @@ public:
     List() {
         head = tail = NULL;
     }
-    
+    void push_back(int val){
+        Node*newNode=new Node(val);
+        if
+    }
     void push_front(int val){
         Node* newNode=new Node(val); 
          if(head==NULL){
