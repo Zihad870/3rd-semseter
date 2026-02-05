64816bcb1df42ca702663ac401cbd2e4e0446c38
Zihad870 <zihadhossen870@gmail.com>
2026-02-05 13:24:00 +0600

    Pre Order Travarsal in Tree

---
+++ DSA/C++_practice_sauda_apu/Binary Tree/preOderTraversal.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+
+Node* buildTree(vector<int>& preorder) {
+    idx++;
+
+    if (preorder[idx] == -1) {
+        return NULL;
+    }
+
+    Node* root = new Node(preorder[idx]);
+    root->left = buildTree(preorder);
+    root->right = buildTree(preorder);
+
+    return root;
+}
+
+void preorderTraversal(Node* root) {
+    if (root == NULL) {
+        return;
+    }
+
+    cout << root->data << " ";
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
+
+int main() {
+    vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+
+   // idx = -1; // important reset
+    Node* root = buildTree(preorder);
+
+    cout << "Preorder Traversal: ";
+    preorderTraversal(root);
+    cout << endl;
+
+    return 0;
+}
